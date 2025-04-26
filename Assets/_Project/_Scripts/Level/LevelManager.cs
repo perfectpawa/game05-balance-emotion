40d3f0343efba53e289c3225ea554aaf439dc83e
perfectpawa <baotlg1005@gmail.com>
2025-04-27 01:11:37 +0700

    logic for piece ratio win condition

--- Assets/_Project/_Scripts/Level/LevelManager.cs
+++ Assets/_Project/_Scripts/Level/LevelManager.cs
@@ -9,7 +9,12 @@ public class LevelManager : ValidatedMonoBehaviour
 {
     [SerializeField, Anywhere] private Board _board;
 
-    [SerializeField] private List<ExplodedPiece> _explodedPieces;
+    [SerializeField] private List<ExplodedPieceData> _explodedPieces;
+    [SerializeField] private List<PieceRatio> _pieceRatioCondition;
+
+    private Dictionary<PieceType, int> _pieceCount;
+    
+    [SerializeField] private bool _ratioWinCondition;
     
     private void OnEnable()
     {
@@ -20,19 +25,27 @@ public class LevelManager : ValidatedMonoBehaviour
         _board.CompleteExplode -= HandleCompleteExplodePiece;
     }
 
-    private void Start()
+    private void Awake()
     {
-        _explodedPieces = new List<ExplodedPiece>();
+        InitValue();
+    }
+
+    private void InitValue()
+    {
+        _explodedPieces = new List<ExplodedPieceData>();
+        _pieceCount = new Dictionary<PieceType, int>();
         
         foreach (var pieceType in Enum.GetValues(typeof(PieceType)).Cast<PieceType>())
         {
             if (pieceType == PieceType.None) continue;
             
-            _explodedPieces.Add(new ExplodedPiece
+            _explodedPieces.Add(new ExplodedPieceData
             {
                 Type = pieceType,
                 Count = 0
             });
+            
+            _pieceCount.Add(pieceType, 0);
         }
     }
 
@@ -45,14 +58,58 @@ public class LevelManager : ValidatedMonoBehaviour
             if (explodedPiece != null && explodedPiece.Type != PieceType.None)
             {
                 explodedPiece.Count += piece.Value;
+                _pieceCount[piece.Key] += piece.Value;
             }
         }
+        
+        CheckPieceRatioWinCondition();
+    }
+
+    private void CheckPieceRatioWinCondition()
+    {
+        _ratioWinCondition = false;
+        
+        if (_pieceRatioCondition.Count < 2)
+        {
+            Debug.LogWarning("Please add at least 2 piece ratio condition");
+            return;
+        }
+        
+        var baseRatio = _pieceCount[_pieceRatioCondition[0].Type] / _pieceRatioCondition[0].Ratio;
+
+        for (var i = 1; i < _pieceRatioCondition.Count; i++)
+        {
+            var count = _pieceCount[_pieceRatioCondition[i].Type];
+
+            if (count == 0)
+            {
+                _ratioWinCondition = false;
+                return;
+            }
+            
+            var currentRatio = count / _pieceRatioCondition[i].Ratio;
+
+            if (!Mathf.Approximately(baseRatio, currentRatio))
+            {
+                _ratioWinCondition = false;
+                return;
+            }
+        }
+
+        _ratioWinCondition = true;
     }
 }
 
 [Serializable]
-public class ExplodedPiece
+public class ExplodedPieceData
 {
     public PieceType Type;
     public int Count;
+}
+
+[Serializable]
+public class PieceRatio
+{
+    public PieceType Type;
+    public float Ratio;
 }
